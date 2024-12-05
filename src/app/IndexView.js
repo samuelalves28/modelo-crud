f1a4fb8f57ba550bce9a10650efebe9953231637
samuelalves28 <83470823+samuelalves28@users.noreply.github.com>
2024-12-04 22:55:24 -0300

    Colando componentes e criando as rotas das telas

---
+++ src/app/IndexView.js
@@ -0,0 +1,12 @@
+// src/app/DashboardPage.js
+import React from 'react';
+
+const IndexView = () => {
+    return (
+        <div>
+            <h1>Bem-vindo ao Dashboard!</h1>
+        </div>
+    );
+};
+
+export default IndexView;
